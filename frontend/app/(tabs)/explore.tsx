fe3bc30dc35560d849858574cf243e53a6cde495
Freddie De Bruyn <freddiedebruyn@Freddies-MacBook-Pro-3.local>
2025-11-07 19:46:31 +0000

    added expo-react-native frontend framework

---
+++ frontend/app/(tabs)/explore.tsx
@@ -0,0 +1,31 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={<Image/>
+      }>
+      <ThemedView className="px-4 pt-4">
+        <ThemedText type='title'>EXPLORE</ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
