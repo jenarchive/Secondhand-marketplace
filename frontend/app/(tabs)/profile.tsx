6d4d6f97f95cb27d3c1e33fdea47d44f1c3f7ac7
Euan <ah24354@bristol.ac.uk>
2026-01-27 13:12:59 +0000

    Added text and avatar in profile page, trying to put in style

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -5,7 +5,7 @@ import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { moderateScale, vertialscale } from "../styles/scaling";
+import { moderateScale, vertialscale } from "../../styles/scaling";
 
 
 const { height } = Dimensions.get('window');
@@ -13,20 +13,60 @@ const { height } = Dimensions.get('window');
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.mainContainer}>
+
     {/* avatar, name and email (with link to change) */}
-    <View style={styles.profileView}>
-      
+    <View style={styles.profileFrame}>
+      <View style ={styles.profileView}> 
+        <Image 
+        style = {styles.profilePhoto}
+        source={require('../../assets/images/avatar.png')} />
+        <View>
+          {/* <ThemedText style={styles.profileUsername}> */}
+            <ThemedText>
+              Username
+            {/* </ThemedText> */}
+          </ThemedText>
+          <ThemedText>
+            Email
+          </ThemedText>
+        </View>
+      </View>
     </View>
     
     {/* purchase history  */}
+    <View style={styles.options}>
+      <ThemedText>
+        purchase history
+      </ThemedText>
+    </View>
 
     {/* liked items */}
+    <View style={styles.options}>
+      <ThemedText >
+        liked items
+      </ThemedText>
+    </View>
 
     {/* reviews */}
+    <View style={styles.options}>
+      <ThemedText>
+        reviews
+      </ThemedText>
+    </View>
 
     {/* current listing (selling list)  */}
+    <View style={styles.options}>
+      <ThemedText>
+        current listing
+      </ThemedText>
+    </View>
 
     {/* settings */}
+    <View style={styles.options}>
+      <ThemedText>
+        settings
+      </ThemedText>
+    </View>
 
     </SafeAreaView>
   );
@@ -38,15 +78,48 @@ const colours = {
 
 const styles = StyleSheet.create({
   mainContainer:{
-    flex:1
+    flex:1, 
+    paddingLeft: moderateScale(51),
+  },
+  profileFrame:{
+    height: moderateScale(170),
+    width: moderateScale(300),
+    borderColor: '#a29191',
+    paddingTop: moderateScale(100),
+    // paddingLeft: moderateScale(51),
+  },
+  profilePhoto:{
+    height: moderateScale(110),
+    width: moderateScale(110),
+    paddingTop: moderateScale(30),
+    paddingLeft: moderateScale(30),
   },
   profileView:{
-    height: moderateScale(80),
-    width: moderateScale(90),
-    borderRadius: 80,
-    borderWidth:1,
-    borderColor: '#a29191',
+
+  },
+  profileUsername:{
+    height: moderateScale(44),
+    width: moderateScale(100),
+    paddingTop: moderateScale(50),
+    paddingLeft: moderateScale(170),
+    alignContent: 'center',
+    justifyContent: 'center',
+    color: '#a29191',
+    
+  },
+  profileEmail:{
+    height: moderateScale(44),
+    width: moderateScale(100),
+    paddingTop: moderateScale(70),
+    paddingLeft: moderateScale(170),
+    alignContent: 'center',
+    justifyContent: 'center',
+    color: '#a29191',
+  },
+  options:{
+    height: moderateScale(70),
+    width: moderateScale(130), 
+    color: '#a29191',
   },
 });
 
-//   <Image source={require('./assets/images/avatar.png')} />
