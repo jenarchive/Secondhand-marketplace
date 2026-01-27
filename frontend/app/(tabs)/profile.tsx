614b7a1446f5b44ce2fe70b655b79c18749d4e08
Euan <ah24354@bristol.ac.uk>
2026-01-27 08:38:26 +0000

    basic structure for profile page

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -4,92 +4,49 @@ import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { moderateScale, vertialscale } from "../styles/scaling";
+
 
 const { height } = Dimensions.get('window');
 
 export default function HomeScreen() {
   return (
-    <ThemedView style={styles.contentContainer}>
+    <SafeAreaView style={styles.mainContainer}>
+    {/* avatar, name and email (with link to change) */}
+    <View style={styles.profileView}>
       
-      {/* welcome header */}
-      <ThemedView style={styles.headerContainer}>
-        <ThemedText type="title">Welcome</ThemedText>
-      </ThemedView>
+    </View>
+    
+    {/* purchase history  */}
 
-      {/* login and sign up buttons */}
-      <View style={styles.authContainer}>
-        
-        {/* Login Button -> Goes to /login */}
-        <Link href="../auth/login" asChild>
-          <Pressable style={styles.secondaryButton}>
-            <ThemedText type="defaultSemiBold" style={{ color: '#fff' }}>Log In</ThemedText>
-          </Pressable>
-        </Link>
+    {/* liked items */}
 
-        {/* Sign Up Button -> Goes to /signup */}
-        <Link href="../auth/signup" asChild>
-          <Pressable style={styles.primaryButton}>
-            <ThemedText type="defaultSemiBold" style={{ color: '#fff' }}>Sign Up</ThemedText>
-          </Pressable>
-        </Link>
+    {/* reviews */}
 
-      </View>
+    {/* current listing (selling list)  */}
 
-    </ThemedView>
+    {/* settings */}
+
+    </SafeAreaView>
   );
 }
 
 const colours = {
-  container: '#25282B',
-  button: '#28289D',
+  container: '#353636',
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
+  mainContainer:{
+    flex:1
   },
-
-  contentContainer: {
-    flex: 1,
-    padding: 24,
-    gap: 32,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  headerContainer: {
-    alignItems: 'center',
-    gap: 8,
-  },
-
-  authContainer: {
-    gap: 16,
-    width: '100%'
-  },
-
-  primaryButton: {
-    backgroundColor: colours.button,
-    paddingVertical: 16,
-    borderRadius: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%'
-  },
-
-  secondaryButton: {
-    backgroundColor: colours.container,
-    paddingVertical: 16,
-    borderRadius: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%'
-  },
-
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  profileView:{
+    height: moderateScale(80),
+    width: moderateScale(90),
+    borderRadius: 80,
+    borderWidth:1,
+    borderColor: '#a29191',
   },
 });
+
+//   <Image source={require('./assets/images/avatar.png')} />
