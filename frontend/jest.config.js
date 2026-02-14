f6373ea58c24f554b9a9a8530464872b962ccdc2
Euan <ah24354@bristol.ac.uk>
2026-02-14 20:19:25 +0000

    updated dependencies to enable testing for frontend

---
+++ frontend/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'react-native',
+
+  transform: {
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
+  },
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
+  ],
+};
