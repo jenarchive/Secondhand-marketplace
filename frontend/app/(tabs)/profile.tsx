acbdf15829ecc3eff7d1fab3307e0282b7e41fc2
Euan <ah24354@bristol.ac.uk>
2026-02-14 00:40:19 +0000

    clean up

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -11,12 +11,9 @@ export default function HomeScreen() {
 
   // list of buttons below profile card
   const Data = [
-    // { id: 1, icon: require('../../assets/images/history.png'), label: "Purchase History", next: require('../../assets/images/next.png'), link: "/items/purchase-history" },
-    { id: 2, icon: require('../../assets/images/heart.png'), label: "Liked Items", next: require('../../assets/images/next.png'), link: "/items/liked-items" },
-    // { id: 3, icon: require('../../assets/images/list.png'), label: "Current Listing", next: require('../../assets/images/next.png'), link: "/items/current-listing" },
-    // { id: 4, icon: require('../../assets/images/notification.png'), label: "Notification", next: require('../../assets/images/next.png'), link: "/items/notification" },
-    { id: 5, icon: require('../../assets/images/settings.png'), label: "Setting", next: require('../../assets/images/next.png'), link: "/items/setting" },
-    { id: 6, icon: require('../../assets/images/door.png'), label: "Log Out", next: require('../../assets/images/next.png'), link: "/items/logout" },
+    { id: 1, icon: require('../../assets/images/heart.png'), label: "Liked Items", next: require('../../assets/images/next.png'), link: "/items/liked-items" },
+    { id: 2, icon: require('../../assets/images/settings.png'), label: "Setting", next: require('../../assets/images/next.png'), link: "/items/setting" },
+    { id: 3, icon: require('../../assets/images/door.png'), label: "Log Out", next: require('../../assets/images/next.png'), link: "/items/logout" },
   ];
 
   return (
@@ -27,7 +24,6 @@ export default function HomeScreen() {
         <Link href="../items/edit-profile" asChild>
           <Pressable style={styles.profileFrame}>
             <ThemedView style={styles.userProfileContainer}>
-              {/* <View style={styles.profileLeftHalf}> */}
                 <ThemedView style={styles.userProfileImage}>
                   <ThemedText type="defaultSemiBold">U</ThemedText>
                 </ThemedView>
@@ -35,30 +31,6 @@ export default function HomeScreen() {
                   <ThemedText type="defaultSemiBold">Username</ThemedText>
                   <ThemedText type="defaultSemiBold">Email</ThemedText>
                 </ThemedView>
-              {/* </View> */}
-
-              {/* <View>
-                <ThemedView style={styles.userRating} accessibilityLabel={`Rating ${4} out of 5`}>
-                  {Array.from({ length: 5 }).map((_, i) => {
-                    const starIndex = i + 1;
-                    const filled = starIndex <= Math.round(4);
-                    return (
-                      <ThemedText
-                        key={starIndex}
-                        type="defaultSemiBold"
-                        style={{ color: filled ? '#FFD700' : '#666', marginHorizontal: 2 }}
-                      >
-                        {filled ? '★' : '☆'}
-                      </ThemedText>
-                    );
-                  })}
-                </ThemedView>
-                <View style={{ alignContent: 'center', alignItems: 'center' }}>
-                  <ThemedText>
-                    8 Reviews
-                  </ThemedText>
-                </View>
-              </View> */}
             </ThemedView>
           </Pressable>
         </Link>
@@ -90,9 +62,6 @@ export default function HomeScreen() {
             </Link>
           )}
         />
-
-        
-
       </View>
     </ThemedView>
 
@@ -106,7 +75,6 @@ const colours = {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    // backgroundColor: '#191C1F',
     paddingLeft: '5%',
     paddingRight: '5%',
     paddingTop: '15.4%',
@@ -127,15 +95,11 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     paddingHorizontal: '5%',
     height: 200,
-    // borderColor: '#FFF',
-    // borderWidth: 1,
   },
 
   profileLeftHalf: {
     alignContent: 'center',
     alignItems: 'center',
-    // borderColor: '#FFF',
-    // borderWidth: 1,
   },
 
   userProfileContainer: {
@@ -149,8 +113,6 @@ const styles = StyleSheet.create({
     padding: 35,
     paddingLeft: 50,
     borderRadius: 50,
-    // borderColor: '#FFF',
-    // borderWidth: 1,
 
     // shadow for IOS
     shadowColor: '#e6e6e6',
@@ -184,16 +146,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    // borderColor: '#cc1111',
-    // borderWidth: 1,
   },
 
   listContainer: {
     paddingBottom: 60,
     flex: 1,
     width: 300,
-    // borderColor: '#e70000',
-    // borderWidth: 1,
     alignContent: 'center',
     alignItems: 'center',
     justifyContent: 'flex-end',
@@ -201,26 +159,16 @@ const styles = StyleSheet.create({
   },
 
   listRow: {
-    // height: 40,
     width: 300,
-    // flexDirection: 'row',
-    // borderColor: '#cc1111',
-    // borderWidth: 1,
-    // backgroundColor: colours.container,
     borderRadius: 25,
-    // paddingHorizontal: 10,
   },
 
   listSide: {
     height: 30,
-    // width: 40,
     flex: 1,
-    // borderRadius: 100,
     alignItems: 'center',
     alignContent: 'center',
     justifyContent: 'center',
-    // borderColor: '#ffffff',
-    // borderWidth: 1,
   },
 
   listIcon: {
@@ -229,11 +177,9 @@ const styles = StyleSheet.create({
   },
 
   listText: {
-    // width: '60%',
     flex: 4,
     marginHorizontal: 25,
     alignContent: 'center',
-    // borderWidth: 1
   },
 
   listArrow: {
