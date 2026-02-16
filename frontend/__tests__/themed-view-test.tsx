942b289e134a6ee75b7b3bf01782b97cc5e2852e
Euan <ah24354@bristol.ac.uk>
2026-02-16 21:23:50 +0000

    first unit test to test themed view

---
+++ frontend/__tests__/themed-view-test.tsx
@@ -0,0 +1,21 @@
+import { render, fireEvent } from '@testing-library/react-native';
+import { ThemedView } from '../components/themed-view';
+import { Appearance, useColorScheme } from 'react-native';
+
+jest.mock('react-native/Libraries/Utilities/useColorScheme');
+
+describe('ThemedView', () => {
+    it('renders correctly in light mode', () => {
+        (useColorScheme as jest.Mock).mockReturnValue('light');
+    const { getByTestId } = render(<ThemedView testID="themed-view" />);
+
+    expect(getByTestId('themed-view')).toHaveStyle({ backgroundColor: '#fff' });
+    });
+
+    it('renders correctly in dark mode', () => {
+        (useColorScheme as jest.Mock).mockReturnValue('dark');
+    const { getByTestId } = render(<ThemedView testID="themed-view" />);
+
+    expect(getByTestId('themed-view')).toHaveStyle({ backgroundColor: '#151718' });
+    });
+})
