c4efe4f23fdb3823382cac4af68bc1ebe2dcb2e4
Jeehyun Lee <dm24602@bristol.ac.uk>
2026-03-08 20:23:10 +0000

    fix bug

--- frontend/app/items/_layout.tsx
+++ frontend/app/items/_layout.tsx
@@ -9,6 +9,7 @@ export default function ItemsLayout() {
     }}>
       <Stack.Screen name="[id]" />
       <Stack.Screen name="current-listing" options={{ title: "My Listings" }} />
+      <Stack.Screen name="edit" options={{ headerShown: false }} />
     </Stack>
   );
 }
